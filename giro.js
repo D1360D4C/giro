0a91a0947bc8bce77886edf39a4ff5f5fc788c79
D1360D4C <diegoarmando117@gmail.com>
2023-08-07 01:40:29 -0300

    actualizacion

--- giro.js
+++ giro.js
@@ -1,46 +1,71 @@
-let imgNormal; // Imagen en su posición normal
-let imgDadaVuelta; // Imagen dada vuelta
-let imagenActual; // Variable para controlar la imagen que se muestra
-let anguloZ = 0; // Ángulo de rotación sobre el eje Z
+let images = []; // Vector para almacenar las imágenes
+let posx=[2];
+let posxX=[2];
+let ro=0;
+let d=-0.4;
+let nose=0;
+let selec=-1;
+let miVariable = "Hola Mundo";
 
 function preload() {
   // Carga las imágenes antes de iniciar el programa
-  imgNormal = loadImage('fondo.jpg');
-  imgDadaVuelta = loadImage('geoo.jpg');
+  
+    images[0]=(loadImage('fondo.jpg'));
+    images.push(loadImage('geoo.jpg'));
+    images.push(loadImage('fondo.jpg'));
+  
 }
 
-
 function setup() {
-  createCanvas(400, 400, WEBGL); // Habilita el modo 3D
-  imagenActual = imgNormal; // Inicia mostrando la imagen normal por defecto
+  createCanvas(650, 400, WEBGL); // Habilita el modo 3D para poder usar rotateY()
+  posx[0]=-200;
+  posx[1]=20;
+  posx[2]=240;
+  posxX[0]=25;
+  posxX[1]=245;
+  posxX[2]=465;
   
 }
 
 function draw() {
-  background(170);
-  //push();
-  noStroke();
-  translate(0, 0, 0); // Asegura que la imagen esté en el plano Z = 0 para evitar efectos de perspectiva
-  rotateY(anguloZ); // Rota el sistema de coordenadas 3D sobre el eje Z
-  if (anguloZ > radians(90) && anguloZ < radians(270) ){
-    imagenActual=imgDadaVuelta;
-  }else{
-      imagenActual=imgNormal;
+  background(200);
+
+  for (let i = 0; i < 3; i++) {
+    push();
+    translate(posx[i] , 0, 0);
+    
+    if(i==selec){
+      
+      nose=nose+radians(2);
+      if(nose<6.3){
+        rotateY(nose); 
+      }else{
+        nose=0;
+        selec=-1
+      }
+    }
+    imageMode(CENTER);
+    image(images[i], 0, 0, 200, 200); // Cambia el tamaño de las imágenes según tus necesidades
+    pop();
+    
+    
   }
-  texture(imagenActual);
-  plane(250);
-  //pop();
-  
-  if(anguloZ > radians(359)){
-    anguloZ = radians(0);
-  }
-  anguloZ += radians(1);
-  
-  
+  document.getElementById("titulo").textContent = selec;
+  document.getElementById("titulo4").textContent = nose;
+  document.getElementById("titulo2").textContent = mouseY;
+  document.getElementById("titulo3").textContent = mouseX;
 }
 
-
-function mouseClicked() {
-  // Rota la imagen sobre el eje Z mientras se arrastra el mouse para lograr el efecto de giro gradual
-  anguloZ -= radians(10); // Incrementa el ángulo de rotación en 5 grados (conversión a radianes)
+function mouseClicked(){
+  if(nose==0){//aca va un if de control por si ya fue seleccionado una carta
+  for(let i=0;i<3;i++){
+    
+    if(mouseY > 100 && mouseY < 300 && mouseX > posxX[i] && mouseX < posxX[i]+200){
+      
+      selec = i;
+      
+    }
+    
+  }
+  }
 }
